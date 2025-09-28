4049b36c337585e0f96fc8de69e0200408314bf2
IliesMomtaz <iliesmomtaz15@gmail.com>
2025-09-28 20:13:50 +0200

    Premiere version du menu + les états du jeu

---
+++ main.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <unistd.h> 
+#include <signal.h>
+#include <string.h>
+#include <termios.h>
+#include <fcntl.h>
+#include "menu.h"
+
+char key_pressed();
+
+int main()
+{
+    menu();
+    char statut = 'M'; // on va avoir trois états : M(Menu), J(Jouer), P(Pause) et Q(Quit)
+
+    while(statut != 'Q'){
+        char result = key_pressed();
+        if(result == 'M' || result == 'm'){ // Si pendant le jeu le joueur veut retourner dans le menu, en le remettant ici il le peut
+            statut = 'M';
+            menu();
+            result = 0; // je remet a chaque fois a 0 pcq sinon pour le pause/play ca posait probleme
+        }
+        if(statut = 'M'){
+            if(result == '1' || result == '2'){
+                statut = 'J';
+                printf("Le Jeu à commencer !!\n");
+                if(result == '1'){
+                    //activer le mode Fluide
+                    result=0;
+                }
+            if(result == '2'){
+                //activer le mode Chargééééééééééééééééééééééééééééééééééééééééééééééé
+                result=0;
+            }
+        }
+        }
+
+        if(statut == 'J'){
+            if(result == 'P' || result == 'p'){
+                statut = 'P';
+                printf("==========LE JEU EST EN PAUSE==========\n");
+                result=0;
+            }
+        }
+
+        if(statut == 'P'){
+            if(result == 'P' || result == 'p'){
+                statut = 'J';
+                printf("=============LE JEU REPREND============\n");
+                result=0;
+            }
+        }
+        
+        if(result == 'Q' || result == 'q'){
+            statut = 'Q';
+            result=0;
+        }
+    }
+    return 0;
+}
+
+
+char key_pressed()
+{
+    struct termios oldterm, newterm;
+    int oldfd; char c, result = 0;
+    
+    tcgetattr (STDIN_FILENO, &oldterm);
+    newterm = oldterm; newterm.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr (STDIN_FILENO, TCSANOW, &newterm);
+    oldfd = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl (STDIN_FILENO, F_SETFL, oldfd | O_NONBLOCK);
+    c = getchar();
+    tcsetattr (STDIN_FILENO, TCSANOW, &oldterm);
+    fcntl (STDIN_FILENO, F_SETFL, oldfd);
+
+    if (c != EOF) {
+        ungetc(c, stdin); 
+        result = getchar();
+    }
+    return result;
+}
+
+
+
+
+
+// Trucs à corriger : quand on la ce le programme et qu'on appui sur 1 par exemple, le chiffre apparait quand le message "Le jeu..." arrive
+// à voir si ca le refait quand y'aura la vraie interface
+// quand on appui sur q pour quitter juste enlever le pourcentage
---
+++ menu.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include "menu.h"
+
+
+void menu()
+{
+    printf("\n");
+    printf("======================================================\n");
+    printf("                Simulateur de parking                 \n");
+    printf("======================================================\n");
+    printf("\n");
+    printf("[1] Activation du mode Fluide\n");
+    printf("\n");
+    printf("[2] Activation du mode Chargé\n");
+    printf("\n");
+    printf("[q] Quitter\n");
+    printf("\n");
+}
+
+
+
