11de55e0adb5312287c3217dc9fcbfd57d7dbed0
timgrm <tmfasim.germain@gmail.com>
2025-12-03 21:22:05 +0100

    Update fonctions.h

--- Include/fonctions.h
+++ Include/fonctions.h
@@ -5,5 +5,11 @@
 int is_free(int x, int y, int l, int h);
 void free_area(int x, int y, int l, int h);
 void occupy_area(int x, int y, int l, int h);
+VEHICULE* vehicle_create(char direction);
+void add_vehicle(VEHICULE** head, VEHICULE* v);
 
 #endif
+
+
+
+
